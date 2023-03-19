f602f0ff5e02ea1a612de35b69c401d49f2651c9
Mubassir Hasan <mubassir913@gmail.com>
2023-03-19 23:08:19 +0600

    fix

--- deb/entrypoint.sh
+++ deb/entrypoint.sh
@@ -34,16 +34,15 @@ if [[ $1 == init ]]; then
 
 else
 
-    # socat will make the conn appear to come from 127.0.0.1
+     # socat will make the conn appear to come from 127.0.0.1
     # ProtonMail Bridge currently expects that.
     # It also allows us to bind to the real ports :)
-    #socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
-    #socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+    socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+    socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
 
     # Start protonmail
     # Fake a terminal, so it does not quit because of EOF...
-    #rm -f faketty
-    #mkfifo faketty
-    #cat faketty | protonmail-bridge --noninteractive
-    protonmail-bridge --noninteractive
+    rm -f faketty
+    mkfifo faketty
+    cat faketty | protonmail-bridge --cli
 fi
