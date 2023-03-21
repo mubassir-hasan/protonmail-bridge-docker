9dcd127439ac50c7c6cd7d6d65421e9b0969a021
Mubassir Hasan <mubassir913@gmail.com>
2023-03-22 02:40:31 +0600

    removed unnecess

--- deb/install.sh
+++ deb/install.sh
@@ -28,37 +28,6 @@ debsig-verify ${DEB_FILE}
 
 gdebi ${DEB_FILE} -n
 
-SERVICE_NAME="protonmail-service"
-
-# Check if the service exists
-if ! systemctl status $SERVICE_NAME &> /dev/null; then
-  echo "Service $SERVICE_NAME does not exist. Creating service..."
-  
-  # Create the service file
-  echo "[Unit]
-  Description=Protonmail bridge service
-  After=network.target
-
-  [Service]
-  Type=simple
-  StandardOutput=journal
-  ExecStart=/usr/bin/protonmail-bridge --noninteractive
-  Restart=always
-
-  [Install]
-  WantedBy=multi-user.target" > /etc/systemd/system/$SERVICE_NAME.service
-
-  # Reload the systemd daemon
-  systemctl daemon-reload
-
-  # Enable and start the service
-  systemctl enable $SERVICE_NAME
-  systemctl start $SERVICE_NAME
-  
-  echo "Service $SERVICE_NAME created and started."
-else
-  echo "Service $SERVICE_NAME already exists."
-fi
 
 # Cleanup
 apt-get purge -y wget 
