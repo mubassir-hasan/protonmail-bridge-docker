8572fe049d81368e116ae7d50a953a958c8db50e
Mubassir Hasan <mubassir913@gmail.com>
2023-03-23 01:16:45 +0600

    imap port configured for idleclient

--- deb/entrypoint.sh
+++ deb/entrypoint.sh
@@ -38,8 +38,9 @@ else
     # ProtonMail Bridge currently expects that.
     # It also allows us to bind to the real ports :)
     socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
-    socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
-
+    #socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+    # Use socat to transfer data between the IMAP server and the local publish port
+    socat TCP4-LISTEN:143,fork SSL:127.0.0.1:1143,verify=0  EXEC:"echo 'IDLE' ; sleep 1 ; echo DONE",pty,ctty 
     # Start protonmail
     # Fake a terminal, so it does not quit because of EOF...
     rm -f faketty
