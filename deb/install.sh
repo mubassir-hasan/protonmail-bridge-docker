0c36a91c7d6ee3863faafcee33f7bbea86c9ef64
Mubassir Hasan <mubassir913@gmail.com>
2024-08-29 23:12:26 +0600

    small

--- deb/install.sh
+++ deb/install.sh
@@ -7,31 +7,30 @@ DEB_FILE=protonmail-bridge_${VERSION}_amd64.deb
 
 # Install dependents
 apt-get update
-apt-get install -y --no-install-recommends debsig-verify debian-keyring gdebi-core ncat pass #socat
+apt-get install -y --no-install-recommends socat pass ca-certificates
 
 # Build time dependencies
-apt-get install -y wget dbus-x11
-8 | 4 | apt-get install gnome-keyring -y
-mkdir  deb
+apt-get install -y wget binutils xz-utils
+
+# Repack deb (remove unnecessary dependencies)
+mkdir deb
 cd deb
-wget -q https://protonmail.com/download/bridge_pubkey.gpg
-gpg --dearmor --output debsig.gpg bridge_pubkey.gpg
-mkdir -p /usr/share/debsig/keyrings/E2C75D68E6234B07
-mv debsig.gpg /usr/share/debsig/keyrings/E2C75D68E6234B07
-
-wget -q https://protonmail.com/download/bridge.pol
-mkdir -p /etc/debsig/policies/E2C75D68E6234B07
-cp bridge.pol /etc/debsig/policies/E2C75D68E6234B07
-
 wget -q https://protonmail.com/download/bridge/${DEB_FILE}
-debsig-verify ${DEB_FILE}
+ar x -v ${DEB_FILE}
+mkdir control
+tar zxvf control.tar.gz -C control
+sed -i "s/^Depends: .*$/Depends: libgl1, libc6, libsecret-1-0, libstdc++6, libgcc1/" control/control
+cd control
+tar zcvf ../control.tar.gz .
+cd ../
+ar rcs -v ${DEB_FILE} debian-binary control.tar.gz data.tar.gz
+cd ../
 
-gdebi ${DEB_FILE} -n
-
-
+# Install protonmail bridge
+apt-get install -y --no-install-recommends ./deb/${DEB_FILE}
 
 # Cleanup
-apt-get purge -y wget 
+apt-get purge -y wget binutils xz-utils
 apt-get autoremove -y
 rm -rf /var/lib/apt/lists/*
 rm -rf deb
