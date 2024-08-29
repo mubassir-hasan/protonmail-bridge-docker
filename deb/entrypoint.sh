96f39da08f3159de5397d435500be39587ce2aa8
Mubassir Hasan <mubassir913@gmail.com>
2024-08-29 23:43:54 +0600

    upgraded to new version

--- deb/entrypoint.sh
+++ deb/entrypoint.sh
@@ -26,24 +26,24 @@ if [[ $1 == init ]]; then
     # done
 
     # Initialize pass
-    gpg --batch --passphrase '' --quick-gen-key 'ProtonMail Bridge' default default never
-    pass init "ProtonMail Bridge"
+    gpg --generate-key --batch /protonmail/gpgparams
+    pass init pass-key
 
     # Login
     protonmail-bridge --cli
 
 else
 
-     # socat will make the conn appear to come from 127.0.0.1
+    # socat will make the conn appear to come from 127.0.0.1
     # ProtonMail Bridge currently expects that.
     # It also allows us to bind to the real ports :)
-    #socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
-    #socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
-    ncat -kl 25 --sh-exec 'nc 127.0.0.1 1025' &
-    ncat -kl 143 --sh-exec 'nc 127.0.0.1 1143' &
+    socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+    socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+
     # Start protonmail
     # Fake a terminal, so it does not quit because of EOF...
     rm -f faketty
     mkfifo faketty
     cat faketty | protonmail-bridge --cli
+
 fi
